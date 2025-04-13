b34beac8c702a7f654cc6a8043eeee84df02e789
MSI\user <claire528816@gmail.com>
2025-04-13 11:23:48 +0800

    fix: 1.修正登入api.js / request.js

--- src/service/request.js
+++ src/service/request.js
@@ -1,72 +1,70 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export function request(config) {
-  config.headers.token = !localStorage.getItem('userInfo')
-    ? ''
-    : JSON.parse(localStorage.getItem('userInfo')).token
+  config.headers.token = !localStorage.getItem('token') ? '' : JSON.parse(localStorage.getItem('token')).token;
   const service = axios.create({
     headers: config.headers,
     baseURL: import.meta.env.MODE === 'production' ? import.meta.env.VITE_BASE_API : '/api',
     timeout: 30000,
     transformRequest: [
       (data = config.params) => {
-        const tempData = getJwtData(JSON.stringify(data))
-        return tempData.data
+        const tempData = getJwtData(JSON.stringify(data));
+        return tempData.data;
       }
     ]
-  })
-  const getJwtData = (data) => {
+  });
+  const getJwtData = data => {
     for (const key in data) {
       if (data.hasOwnProperty(key)) {
-        const val = data[key]
+        const val = data[key];
         if (val === '' || val === undefined) {
-          delete data[key]
+          delete data[key];
         }
       }
     }
-    const jwt = encodeURIComponent(btoa(encodeURIComponent(data)))
-    return { data: jwt }
-  }
+    const jwt = encodeURIComponent(btoa(encodeURIComponent(data)));
+    return { data: jwt };
+  };
 
   // request攔截
   service.interceptors.request.use(
-    (config) => {
-      return config
+    config => {
+      return config;
     },
-    (error) => {
-      console.log(error)
-      Promise.reject(error)
+    error => {
+      console.log(error);
+      Promise.reject(error);
     }
-  )
+  );
 
   // response攔截
   service.interceptors.response.use(
-    (response) => {
-      return response
+    response => {
+      return response;
     },
-    (error) => {
+    error => {
       // 這裡處理具體的錯誤代碼
       if (error.response) {
         switch (error.response.status) {
           case 400:
-            console.error('400 Bad Request')
-            break
+            console.error('400 Bad Request');
+            break;
           case 404:
-            console.error('404 Not Found')
-            break
+            console.error('404 Not Found');
+            break;
           case 500:
-            console.error('500 Internal Server Error')
-            break
+            console.error('500 Internal Server Error');
+            break;
           default:
-            console.error(`Error: ${error.response.status}`)
+            console.error(`Error: ${error.response.status}`);
         }
       } else {
-        console.error('Network Error or Request Timed Out')
+        console.error('Network Error or Request Timed Out');
       }
-      return Promise.reject(error)
+      return Promise.reject(error);
     }
-  )
-  return service(config)
+  );
+  return service(config);
 }
 
-console.log(import.meta.env.MODE)
+console.log(import.meta.env.MODE);
