6e22b1872f65ca4948b82dd9888f877cf53da2c3
佳沛的PC\claire <claire528816@gmail.com>
2025-10-19 11:22:28 +0800

    1.定期更新

--- src/views/goods/GoodsTypeList.vue
+++ src/views/goods/GoodsTypeList.vue
@@ -1,89 +1,81 @@
 <template>
-  <div>
-    <!-- 搜尋與新增按鈕 -->
-    <div class="flex justify-between p-3 bg-white rounded">
-      <div>
-        <el-input v-model="searchKeyword" placeholder="搜尋分類名稱" clearable />
-        <el-button type="primary" icon="Search" @click="getGoodsTypeList">搜尋</el-button>
+  <div class="p-5">
+    <div class="flex items-end justify-between p-3 my-3 bg-white rounded bd-1">
+      <div class="flex items-center gap-2">
+        <div class="w-[140px]">
+          <el-input v-model="searchKeyword" placeholder="搜尋分類名稱" clearable />
+
+        </div>
       </div>
-      <el-button
-        class="btn--create"
-        plain
-        icon="Plus"
-        @click="showAddDialog = true"
-      >
+
+      <el-button type="primary" icon="Search" @click="getGoodsTypeList">搜尋</el-button>
+      <el-button class="btn--create" plain icon="Plus" @click="showAddDialog = true">
         新增分類
       </el-button>
     </div>
 
-    <!-- 表格 -->
-    <el-table :data="tableData" style="width: 100%" v-loading="loading">
-      <el-table-column prop="ID" label="ID" width="80" />
-      <el-table-column prop="Name" label="分類名稱" />
-      <el-table-column label="操作" width="160">
-        <template #default="{ row }">
-          <el-button size="small" @click="openEditDialog(row)">編輯</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+    <!--表格內容-->
+    <div class="flex items-end justify-between p-3 my-3 bg-white rounded bd-1">
+      <el-table :data="tableData" style="width: 100%" v-loading="loading">
+        <el-table-column prop="ID" label="ID" width="80" />
+        <el-table-column prop="Name" label="分類名稱" />
+        <el-table-column label="操作" width="160">
+          <template #default="{ row }">
+            <el-button size="small" @click="clickOpenEditModal(row)">編輯</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
 
-    <!-- 新增 Dialog -->
-    <GoodsTypeAddModal
-      v-model:visible="showAddDialog"
-      @confirm="onAddSuccess"
-      @close="showAddDialog = false"
-    />
+      <!-- 新增 Dialog -->
+      <GoodsTypeAddModal v-model:visible="showAddDialog" @confirm="handleAddSuccess" @close="showAddDialog = false" />
 
-    <!-- 編輯 Dialog -->
-    <GoodsTypeEditModal
-      v-model:visible="showEditDialog"
-      :editData="editForm"
-      @confirm="onEditSuccess"
-      @close="showEditDialog = false"
-    />
+      <!-- 編輯 Dialog -->
+      <GoodsTypeEditModal v-model:visible="showEditDialog" :editData="editForm" @confirm="handleEditSuccess"
+        @close="showEditDialog = false" />
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import { getGoodsType } from '@/service/api'
+import { useGoodsTypeList } from './composables'
 import { ElMessage } from 'element-plus'
 import GoodsTypeAddModal from './components/GoodsTypeAddModal.vue'
 import GoodsTypeEditModal from './components/GoodsTypeEditModal.vue'
 
-const tableData = ref([])
-const loading = ref(false)
-const searchKeyword = ref('')
-const showAddDialog = ref(false)
-const showEditDialog = ref(false)
-const editForm = ref({})
+const {
+  searchFilter,
+  tableData,
+  tableLoading,
+  goodsForm,
+  goodsTypeList,
+  getGoodsTypeName,
+  getGoodsListRequest,
+  getGoodsTypeList,
+} = useGoodsTypeList()
 
-const getGoodsTypeList = async () => {
-  loading.value = true
-  try {
-    const res = await getGoodsType({})
-    if (res?.data?.Code === 200) {
-      tableData.value = res.data.Data
-    }
-  } finally {
-    loading.value = false
-  }
+const showAddModal = ref(false);
+const showEditModal = ref(false);
+const editForm = ref({});
+//---------------------------------------------------------
+/** 點擊開啟【編輯】彈跳視窗 */
+const clickOpenEditModal = (row) => {
+  editForm.value = { ...row };
+  showEditModal.value = true;
 }
 
-const openEditDialog = (row) => {
-  editForm.value = { ...row }
-  showEditDialog.value = true
-}
+/** 處理【新增】成功 */
+const handleAddSuccess = () => {
+  ElMessage.success('新增成功');
+  showAddModal.value = false;
+  getGoodsTypeList();
+};
 
-const onAddSuccess = () => {
-  ElMessage.success('新增成功')
-  getGoodsTypeList()
-}
-
-const onEditSuccess = () => {
-  ElMessage.success('更新成功')
-  getGoodsTypeList()
-}
+/** 處理【編輯】成功 */
+const handleEditSuccess = () => {
+  ElMessage.success('更新成功');
+  getGoodsTypeList();
+};
 
 onMounted(() => {
   getGoodsTypeList()
